c85b7660d7d429b70b326f059f0384945cf7a26a
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2022-09-29 21:40:21 +0700

    arif

--- register.php
+++ register.php
@@ -12,13 +12,13 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 bg-primary py-4 text-center shadow-lg">
-          <h3 class="text-white mb-4">READ AND LEARN WITH US.</h3>
+          <h4 class="text-white mb-4">JOIN READ AND LEARN FOR YOUR FUTURE.</h4>
           <img src="img/pohon.png" width="80%">
         </div>
         <div class="col-lg-6 bg-white d-flex justify-content-center text-gray">
           <form action="proses_login.php" method="post">
-            <h2 class="text-primary mt-5 mb-3">Login</h2>
-            <p class="mb-5 fs-7">Don't have an account? <a href="register.php">Create account</a></p>
+            <h2 class="text-primary mt-5 mb-3">Form Registration</h2>
+            <p class="mb-5 fs-7">Already have an account? <a href="login.php">Login here</a></p>
             <div class="mb-4 mbe" style="position: relative;">
               <input type="text" class="form-control-s" id="exampleInputEmail1" name="user" required>
               <i class="bi bi-person-check-fill"></i>
@@ -27,13 +27,14 @@
             <div class="mb-4 mbe" style="position: relative;">
               <input type="password" class="form-control-s" id="exampleInputPassword1" name="pass" required>
               <i class="bi bi-shield-lock-fill"></i>
-              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <label for="exampleInputPassword1" class="form-label">New Password</label>
             </div>
-            <div class="mb-4 position-relative fs-7">
-              <input type="checkbox" class="custom-control-input" id="customCheck">
-              <label class="custom-control-label" for="customCheck">Remember Me</label>
+            <div class="mb-4 mbe" style="position: relative;">
+              <input type="password" class="form-control-s" id="exampleInputPassword1" name="pass2" required>
+              <i class="bi bi-shield-lock-fill"></i>
+              <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
             </div>
-            <button type="submit" class="btn btn-primary btn-sm mb-5 rounded-pill shadow px-4 mx-auto">Login</button>
+            <button type="submit" class="btn btn-primary btn-sm mb-5 rounded-pill shadow px-4 mx-auto">Register</button>
             <div class="copyright">
               <p style="font-size: 12px;">Copyright &copy; 2022, Perpustakaan K-9 | Kharisma Academy</p>
             </div>
