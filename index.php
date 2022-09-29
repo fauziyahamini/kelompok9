2021dc01f94c7e4ce4d73418e6349f5c66844e0b
asaddin02 <114129501+asaddin02@users.noreply.github.com>
2022-09-29 13:08:42 +0700

    kelompok9

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.rtl.css">
+    <title>Perpus K-9</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 text-center">
+                <h1>LOGO</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
