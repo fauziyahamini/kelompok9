c46dbaee51e3d283e0b54192ec9e32631edd4ddf
fauziyahamini <fauziyahamini98@gmail.com>
2022-09-29 19:57:27 +0700

    fauziyah

--- index.php
+++ index.php
@@ -10,52 +10,31 @@
     <title>Perpus K-9</title>
 </head>
 <body>
-    <!-- <div class="container-fluid">
-        <div class="row justify-content-center">
-            <div class="col-lg-12 text-center">
-                <h1>LOGO</h1>
-            </div>
-        </div>
-    </div> -->
-    <!-- <nav class="navbar bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="img/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
-      Perpustakaan
-    </a>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
-      </div>
-  </div>
-</nav> -->
-
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-  <img src="img/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+<nav class="navbar navbar-expand-lg " style="background-color: #e3f2fd;">
+  <div class="container">
+  <img src="img/logo.png" alt="Logo" width="50" height="50" class=" align-text-top">
     <a class="navbar-brand" href="#">PERPUSTAKAAN</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">About Us</a>
-        <a class="nav-link" href="#">Daftar Buku</a>
-        <button class="btn btn-primary" type="submit">Login</button>
-        <!-- <i class="box-arrow-in-right"></i> -->
-      </div>
-    </div>
-  </div>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 pe-3">
+            <li class="nav-item">
+            <a class="nav-link active" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active ">Daftar Buku</a>
+            </li>
+        </ul>
+        <button class="d-flex btn btn-primary" type="submit">Login <span class="bi-box-arrow-in-right"></span></button>
 </nav>
+
 <!-- navbar -->
     <!-- carousel -->
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/images.png" class="d-block w-100" alt="...">
+      <img src="img/book1.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="img/book.jfif" class="d-block w-100" alt="...">
