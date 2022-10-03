910c76225f287518f16ede4b9b37297f5f951ddf
fauziyahamini <fauziyahamini98@gmail.com>
2022-10-03 14:35:03 +0700

    fauziyah

--- data_siswa.php
+++ data_siswa.php
@@ -53,10 +53,10 @@ if(isset($_GET['cari'])){
                         
 
                         }else{
-                        // $ambil = mysqli_query($db,"SELECT siswa.nis,siswa.nama,siswa.jenis_kelamin,siswa.alamat,kelas.nama_kelas from siswa inner join kelas on siswa.id_kelas=kelas.id_kelas");
+                        $ambil = mysqli_query($db,"SELECT siswa.nis,siswa.nama,siswa.jenis_kelamin,siswa.alamat,kelas.nama_kelas from siswa inner join kelas on siswa.id_kelas=kelas.id_kelas");
                         
-                        // $jumlah_data = mysqli_num_rows($ambil);
-				        // $total_halaman = ceil($jumlah_data / $batas);
+                        $jumlah_data = mysqli_num_rows($ambil);
+				        $total_halaman = ceil($jumlah_data / $batas);
 
                         $ambil = mysqli_query($db,"SELECT siswa.nis,siswa.nama,siswa.jenis_kelamin,siswa.alamat,kelas.nama_kelas from siswa inner join kelas on siswa.id_kelas=kelas.id_kelas limit $halaman_awal, $batas");
                         $nomor = $halaman_awal+1;
@@ -92,93 +92,23 @@ if(isset($_GET['cari'])){
                     </table>
                     </form>
 
-                    <div align="right">
-  <ul class="pagination">
+                    <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item disabled">
+      <a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$previous'"; } ?>>Previous</a>
+    </li>
+    <?php 
+	for($x=1;$x<=$total_halaman;$x++){
+	?> 
+    <li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
     <?php
-      // Jika page = 1, maka LinkPrev disable
-      if($page == 1){ 
-    ?>        
-      <!-- link Previous Page disable --> 
-      <li class="disabled"><a href="#">Previous</a></li>
-    <?php
-      }
-      else{ 
-        $LinkPrev = ($page > 1)? $page - 1 : 1;  
-
-        if($kolomCari=="" && $kolomKataKunci==""){
-        ?>
-          <li><a href="index.php?page=<?php echo $LinkPrev; ?>">Previous</a></li>
-     <?php     
-        }else{
-      ?> 
-        <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $LinkPrev;?>">Previous</a></li>
-       <?php
-         } 
-      }
-    ?>
-
-    <?php
-      //kondisi jika parameter pencarian kosong
-      if($kolomCari=="" && $kolomKataKunci==""){
-        $SqlQuery = mysqli_query($con, "SELECT * FROM Siswa");
-      }else{
-        //kondisi jika parameter kolom pencarian diisi
-        $SqlQuery = mysqli_query($con, "SELECT * FROM Siswa WHERE $kolomCari LIKE '%$kolomKataKunci%'");
-      }     
-    
-      //Hitung semua jumlah data yang berada pada tabel Sisawa
-      $JumlahData = mysqli_num_rows($SqlQuery);
-      
-      // Hitung jumlah halaman yang tersedia
-      $jumlahPage = ceil($JumlahData / $limit); 
-      
-      // Jumlah link number 
-      $jumlahNumber = 1; 
-
-      // Untuk awal link number
-      $startNumber = ($page > $jumlahNumber)? $page - $jumlahNumber : 1; 
-      
-      // Untuk akhir link number
-      $endNumber = ($page < ($jumlahPage - $jumlahNumber))? $page + $jumlahNumber : $jumlahPage; 
-      
-      for($i = $startNumber; $i <= $endNumber; $i++){
-        $linkActive = ($page == $i)? ' class="active"' : '';
-
-        if($kolomCari=="" && $kolomKataKunci==""){
-    ?>
-        <li<?php echo $linkActive; ?>><a href="index.php?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
-
-    <?php
-      }else{
-        ?>
-        <li<?php echo $linkActive; ?>><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
-        <?php
-      }
-    }
-    ?>
-    
-    <!-- link Next Page -->
-    <?php       
-     if($page == $jumlahPage){ 
-    ?>
-      <li class="disabled"><a href="#">Next</a></li>
-    <?php
-    }
-    else{
-      $linkNext = ($page < $jumlahPage)? $page + 1 : $jumlahPage;
-     if($kolomCari=="" && $kolomKataKunci==""){
-        ?>
-          <li><a href="index.php?page=<?php echo $linkNext; ?>">Next</a></li>
-     <?php     
-        }else{
-      ?> 
-         <li><a href="index.php?Kolom=<?php echo $kolomCari;?>&KataKunci=<?php echo $kolomKataKunci;?>&page=<?php echo $linkNext; ?>">Next</a></li>
-    <?php
-      }
-    }
-    ?>
+	}
+	   ?>	
+    <li class="page-item">
+      <a class="page-link" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?> >Next</a>
+    </li>
   </ul>
-</div>
+</nav>
     </div>
 </main>
 <?php include "footer.php"; ?>
