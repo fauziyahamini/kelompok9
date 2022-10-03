9669f4d4384507bc114607f2ff72127022aa1a30
fauziyahamini <fauziyahamini98@gmail.com>
2022-10-03 15:04:42 +0700

    fauziyah

--- edit.php
+++ edit.php
@@ -61,44 +61,82 @@ if(isset($_POST['update']))
   <div class="container-fluid">
   <div class="container mt-5">
 <form action="" method="POST" enctype="multipart/form-data"> 
-  <div class="mb-3">
-    <label class="form-label">ID Buku</label>
-    <input type="number" class="form-control" name="id_buku" value="<?php echo $_GET['id'];?>" placeholder="">
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Penulis</label>
-    <input type="text" class="form-control"  value="<?php echo $data['penulis'];?>" name="penulis" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Tahun</label>
-    <input type="number" class="form-control" value="<?php echo $data['tahun'];?>" name="tahun" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Judul</label>
-    <input type="text" class="form-control" value="<?php echo $data['judul'];?>" name="judul" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Kota</label>
-    <input type="text" class="form-control" value="<?php echo $data['kota'];?>" name="kota" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Penerbit</label>
-    <input type="text" class="form-control" value="<?php echo $data['penerbit'];?>" name="penerbit" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Cover</label>
-    <img src="cover/<?php echo $data['cover'];?>" alt="" style="width:50px;">
-    <input type="file" class="form-control" name="cover" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Sinopsis</label>
-    <input type="text" class="form-control" value="<?php echo $data['sinopsis'];?>" name="sinopsis" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Stok</label>
-    <input type="number" class="form-control" value="<?php echo $data['stok'];?>" name="stok" >
-  </div>
-  <button type="submit" class="btn btn-primary" name="update">UPDATE</button>
+
+<div class="card  shadow">
+      <div class="card-header ">
+        Form Tambah Buku
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group mb-2">
+              <label class="form-label">ID Buku</label>
+              <input type="number" class="form-control" name="id_buku" value="<?php echo $_GET['id'];?>" placeholder="Masukan ID Buku">
+              </div>
+              </div>
+            <div class="col-md-6">
+            <div class="form-group">
+              <label class="form-label">Penulis</label>
+              <input type="text" class="form-control" value="<?php echo $data['penulis'];?>" name="penulis" >
+            </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+          <div class="form-group">
+            <label class="form-label">Tahun</label>
+            <input type="number" class="form-control" value="<?php echo $data['tahun'];?>" name="tahun" >
+          </div>
+              </div>
+            <div class="col-md-6">
+            <div class="form-group">
+            <label class="form-label">Judul</label>
+            <input type="text" class="form-control" value="<?php echo $data['judul'];?>" name="judul" >
+          </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+          <div class="form-group">
+            <label class="form-label">Kota</label>
+            <input type="text" class="form-control"  value="<?php echo $data['kota'];?>" name="kota" >
+          </div>
+              </div>
+            <div class="col-md-6">
+            <div class="form-group">
+            <label class="form-label">Penerbit</label>
+            <input type="text" class="form-control" value="<?php echo $data['penerbit'];?>"  name="penerbit" >
+          </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-6">
+          <div class="form-group">
+            <label class="form-label">Cover</label>
+            <img src="cover/<?php echo $data['cover'];?>" alt="" style="width:50px;">
+            <input type="file" class="form-control" name="cover" >
+          </div>
+              </div>
+            <div class="col-md-6">
+            <div class="form-group">
+            <label class="form-label">Stok</label>
+            <input type="text" class="form-control" value="<?php echo $data['stok'];?>" name="stok" >
+          </div>
+          </div>
+      </div>
+      <div class="row">
+        <div class="form-group">
+            <label class="form-label">Sinopsis</label>
+            <textarea class="form-control" name="sinopsis" value="<?php echo $data['sinopsis'];?>" rows="5"></textarea>
+          </div>
+      </div>
+    </div>
+    <div class="card-footer">
+       <button type="submit" name="update" class="btn btn-primary float-right">Update</button>
+        </div>
+               
+   </div>
+  
 </form>
 </div>
   </div>
