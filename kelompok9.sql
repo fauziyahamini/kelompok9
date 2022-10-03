dd72c233005bd54382dc9cb3096fd61e7d58c5ae
fauziyahamini <fauziyahamini98@gmail.com>
2022-10-03 21:09:45 +0700

    fauziyah

--- kelompok9.sql
+++ kelompok9.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.3.0-dev+20220928.000bf397a4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Okt 2022 pada 05.40
+-- Waktu pembuatan: 03 Okt 2022 pada 16.08
 -- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.4
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,10 +44,24 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `penulis`, `tahun`, `judul`, `kota`, `penerbit`, `cover`, `sinopsis`, `stok`) VALUES
-(32, '4fdsdf', 2022, 'sfdad', 'fdzs', 'zdf', '1.png', 'adsfd', 32),
+(32, '4fdsdf', 2022, 'bukuu', 'fdzs', 'zdf', '1.png', 'aaaa', 32),
 (324, 'dsa', 2022, 'sadfd', 'f', 'f', '3.png', 'sdfad', 21),
 (32413, 'dfsafd', 2022, 'xcvzc', 'xzvc', 'xvz', '1.png', 'sdafsdf', 12),
 (32443, 'dsvds', 2022, 'dsfsd', 'sadfasdf', 'sadfd', '4.png', 'sdfsadf', 231),
+(155101, 'Pramoedya Ananta Toer', 2010, 'Rumah Kaca', 'Jakarta', 'Lentera Dipantera', '1.png', 'Lentera Dipantera', 4),
+(155102, '?Andrea Hirata', 2010, '?Laskar Pelangi', 'Jakarta', 'Bentang Pustaka', '2.png', 'Laskar pelangi merupakan?novel fiksi?yang ditulis oleh Andrea Hirata.?Novel?tersebut mengisahkan tentang kehidupan 10 anak di Pulau Belitung,', 5),
+(155103, 'Tere Liye', 2010, 'Tentang Kamu', 'Jakarta', 'Republika Penerbit', '1.png', 'Tentang Kamu, merupakan novel karya Tere Liye. Novel tersebut menceritakan kisah hidup wanita dari keluarga miskin yang tinggal di Pulau Bungin,', 6),
+(155104, 'Dhonny Dhirgantoro', 2010, '5 Cm', 'Jakarta', 'PT. Grasindo', '2.png', '5 Cm, merupakan novel yang mengisahkan tentang lima sahabat bernama Zafran, Arial, Ian, Genta dan seorang wanita bernama Riani.', 7),
+(155105, 'Himawan Pratists', 2011, 'Memahami Film', 'Bandung', 'Homerian Pustaka', '1.png', 'Masih banyak orang yang melihat film sebagai sesuatu yang mustahil untuk dijalankan. Film bisa dibuat oleh siapa saja dan banyak yang berminat untuk belajar membuat film.', 8),
+(155106, 'Yuliadi Soekardi', 2011, 'Sangkuriang', 'Bandung', 'CV. Putaka Setia', '2.png', 'Buku ini mengisahkan tentang seorang anak yang mencintai ibunya. Namun, cinta tersebut berwujud cinta antara pria kepada seorang wanita.?', 9),
+(155107, 'Andrea Hirata', 2011, 'Ayah', 'Bandung', 'Bentang Pustaka', '1.png', 'Novel karya Andrea Hirata ini merupakan trilogi dari novel karya beliau yang berjudul Laskar Pelangi.', 10),
+(155108, 'Darwis Tere Liye', 2011, 'Rindu', 'Bandung', 'Republika', '2.png', 'Novel Karya Darwis Tere Liye ini mengisahkan tentang perjalanan panjang kerinduan yang membebani hati.', 11),
+(155109, 'Windry Ramadhina', 2011, 'London', 'Bandung', 'PT. Gramedia Pustaka Utama', '1.png', 'Novel berjudul London ini mengisahkan tentang kehidupan seorang penulis novel romance bernama Gilang.', 12),
+(155110, 'Agnes Davonar', 2015, 'Surat Kecil untuk Tuhan', 'Surabaya', 'Inandra Published', '2.png', 'Novel ini mengisahkan tentang perjuangan seorang gadis untuk melawan kanker ganas.', 13),
+(155111, 'Andrea Hirata', 2016, 'Sang Pemimpi', 'Surabaya', 'Bentang Pustaka', '1.png', 'Novel karya Andrea Hirata ini mengisahkan tentang kehidupan tiga anak Melayu Belitong bernama Arai, Ikal dan Jimbron', 14),
+(155112, 'Ahmad Fuadi', 2017, 'Negeri 5 Menara', 'Surabaya', 'PT. Gramedia Pustaka Utama', '2.png', 'Novel karya Ahmad Fuadi ini sudah difilmkan. Tokoh utama di dalam novel ini adalah Alif. Ia lahir di Minangkabau.', 15),
+(155113, 'Pramoedya Ananta Toer', 2018, 'Rumah Kaca', 'Yogyakarta', 'Lentera Dipantera', '1.png', 'Lentera Dipantera', 16),
+(155114, 'Himawan Pratists', 2011, 'Memahami Film', 'Bandung', 'Homerian Pustaka', '2.png', 'Masih banyak orang yang melihat film sebagai sesuatu yang mustahil untuk dijalankan. Film bisa dibuat oleh siapa saja dan banyak yang berminat untuk belajar membuat film.', 17),
 (234143, 'dsfafav', 2112, 'vcxzvvfds', 'vsvadvavdvsa', 'vdsfadsf', '3.png', 'xcvzcvxzv', 32),
 (322413, 'sdfadsfa', 2022, 'd', 'd', 'dfadsf', '4.png', 'zxvcxzcv', 4332),
 (324134, 'dsfadf', 2004, 'fdsafdsgd', 'fvds', 'zdsvfd', '4.png', 'czxvcxvz', 223),
@@ -80,7 +94,8 @@ INSERT INTO `detail_peminjaman` (`id_detail_peminjaman`, `id_buku`, `id_peminjam
 (14, 32443, 17, 1),
 (15, 324, 18, 1),
 (16, 32443, 19, 1),
-(17, 322413, 20, 1);
+(17, 322413, 20, 1),
+(18, 324, 21, 1);
 
 -- --------------------------------------------------------
 
@@ -146,7 +161,8 @@ INSERT INTO `peminjaman` (`id_peminjaman`, `id_siswa`, `id_petugas`, `tanggal_pe
 (17, 3214234, 7, '2022-10-03', '2022-10-17'),
 (18, 2323323, 7, '2022-10-03', '2022-10-17'),
 (19, 2323323, 1, '2022-10-03', '2022-10-17'),
-(20, 2323323, 1, '2022-10-03', '2022-10-17');
+(20, 2323323, 1, '2022-10-03', '2022-10-17'),
+(21, 3214234, 1, '2022-10-03', '2022-10-17');
 
 -- --------------------------------------------------------
 
@@ -203,13 +219,29 @@ CREATE TABLE `siswa` (
 --
 
 INSERT INTO `siswa` (`nis`, `nama`, `jenis_kelamin`, `alamat`, `id_kelas`) VALUES
-(2314231, 'weqrweq', 'L', 'safsdaf', 4),
+(2314231, 'budi', 'L', 'safsdaf', 1),
 (2323323, 'weedfs', 'P', 'asdsadas', 5),
 (3214234, 'dsafsadf', 'L', 'sdafdsfsafdfas', 6),
 (3223232, 'dsacsz', 'L', 'xczcz', 4),
-(3323232, 'dssdsaas', 'L', 'asaasasasa', 1),
 (4255432, 'werfads', 'L', 'asfdasfd', 5),
-(33443897, 'sdaddasdas', 'L', 'aasdasda', 3);
+(16650101, 'Maulana Alif Anugrah', 'L', 'Lowokwaru', 1),
+(16650102, 'Nadya Saphira', 'P', 'Dau', 1),
+(16650103, 'Feggy Rizkiana Herman?', 'L', 'Batu', 1),
+(16650104, 'Armelia nur asyiffa?', 'P', 'Ketawanggede', 2),
+(16650105, 'Feggy Rizkiana Herman?', 'L', 'Karang Ploso', 2),
+(16650106, 'Hazana Delfani?', 'P', 'Blimbing', 2),
+(16650107, 'Lucky Wiratama Suganda?', 'L', 'Kepanjen', 3),
+(16650108, 'ristami annisa?', 'P', 'Sukun', 3),
+(16650109, 'Yolanda Novitri Setiawan', 'L', 'Wagir', 3),
+(16650110, 'Aulia Rahmawaty?', 'P', 'Dieng', 4),
+(16650111, 'Denis Muhammad Irfan?', 'L', 'Lowokwaru', 4),
+(16650112, 'Rida Himyati Hasna', 'P', 'Dau', 4),
+(16650113, 'Ichsan nurmansyah', 'L', 'Batu', 5),
+(16650114, 'sinta komara?', 'P', 'Ketawanggede', 5),
+(16650115, 'muhammad yusuf?', 'L', 'Karang Ploso', 5),
+(16650116, 'Widi Dwi Adhawati?', 'P', 'Blimbing', 6),
+(16650117, 'Dwi Putri Januari?', 'L', 'Kepanjen', 6),
+(16650118, 'Meisa damayanti', 'P', 'Sukun', 6);
 
 -- --------------------------------------------------------
 
@@ -303,7 +335,7 @@ ALTER TABLE `userr`
 -- AUTO_INCREMENT untuk tabel `detail_peminjaman`
 --
 ALTER TABLE `detail_peminjaman`
-  MODIFY `id_detail_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_detail_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `detail_pengembalian`
@@ -321,7 +353,7 @@ ALTER TABLE `kelas`
 -- AUTO_INCREMENT untuk tabel `peminjaman`
 --
 ALTER TABLE `peminjaman`
-  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT untuk tabel `petugas`
