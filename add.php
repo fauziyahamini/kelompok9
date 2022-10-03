820d37bcc84792b9e928521cd008868ea4b53d67
fauziyahamini <fauziyahamini98@gmail.com>
2022-10-03 12:16:38 +0700

    fauziyah

--- add.php
+++ add.php
@@ -45,14 +45,8 @@ include_once('home_admin.php');
     <title>ADD</title>
 </head>
 <body>
-<<<<<<< HEAD
-<main>
-  <div class="container-fluid px-5">
-=======
-<main class="mb-5">
-  <div class="container-fluid px-4">
->>>>>>> 0e2b960115c0e9135ab9185bc9f82b08ef54ae44
-  <div class="container mt-5">
+
+  <div class="container px-4 mt-5">
    <form action="" method="POST" enctype="multipart/form-data"> 
 
    <!--  -->
@@ -133,9 +127,7 @@ include_once('home_admin.php');
    </div> 
    
 
-    
-  </div>
-</main>
+
 
 
 <?php include "footer.php"; ?>
